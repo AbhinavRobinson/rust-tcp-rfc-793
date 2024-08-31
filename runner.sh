2dd9a6b161afa1fa7c7e1ca78e157e19859d1511
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-08-31 22:12:49 +0530

    fix: script

--- runner.sh
+++ runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cargo b --release
-sudo setcap cap_net_admin=eip ./release/rust-tcp-rfc-793
-./release/rust-tcp-rfc-793 &
+sudo setcap cap_net_admin=eip ./target/release/rust-tcp-rfc-793
+./target/release/rust-tcp-rfc-793 &
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
