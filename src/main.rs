95530bb1a5568fcefac31a8b4edb94e86b24ca63
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-08-31 20:40:12 +0530

    fix: tun linux

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use std::io::Read;
+use std::io;
 
-fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let mut config = tun2::Configuration::default();
-    config
-        .address((10, 0, 0, 9))
-        .netmask((255, 255, 255, 0))
-        .destination((10, 0, 0, 1))
-        .up();
-
-    let mut dev = tun2::create(&config)?;
-    let mut buf = [0; 4096];
-
-    loop {
-        let amount = dev.read(&mut buf)?;
-        println!("{:?}", &buf[0..amount]);
-    }
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("Read {} : {:x?}", nbytes - 4, &buf[0..nbytes]);
+    Ok(())
 }
