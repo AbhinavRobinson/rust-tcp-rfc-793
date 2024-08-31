26eb6a519257b80402a8844e856b44128677f447
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-08-31 22:18:25 +0530

    fix: trap kil

--- runner.sh
+++ runner.sh
@@ -6,6 +6,6 @@ sudo setcap cap_net_admin=eip ./target/release/rust-tcp-rfc-793
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
-
+trap "kill $pid" INT TERM
 wait $pid
 
