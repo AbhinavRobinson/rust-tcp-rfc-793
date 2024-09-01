2baa5a428a788a71172364db07ed16055704c1e2
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-09-01 11:08:26 +0530

    feat: basic errors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let network_interface = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let network_interface =
+        tun_tap::Iface::new("tun0", tun_tap::Mode::Tun).expect("Failed to create Tun interface.");
     let mut buffer = [0u8; 1504];
     loop {
-        let network_bytes = network_interface.recv(&mut buffer[..])?;
+        let network_bytes = network_interface
+            .recv(&mut buffer[..])
+            .expect("Failed to read network bytes.");
         let ether_protocol = u16::from_be_bytes([buffer[2], buffer[3]]);
         if ether_protocol != 0x0800 {
             // ignore packets other than ipv4
@@ -13,7 +16,7 @@ fn main() -> io::Result<()> {
         match etherparse::Ipv4Slice::from_slice(&buffer[4..network_bytes]) {
             Ok(p) => {
                 let header = p.header();
-                let payload_len = header.payload_len().expect("Error parsing payload len");
+                let payload_len = header.payload_len().expect("Error parsing payload lenght.");
                 let source_addr = header.source_addr();
                 let destination_addr = header.destination_addr();
                 let protocol = header.protocol();
