403822bd6a2355b4caf5673ff5d737ed8e66fc75
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-09-01 10:28:38 +0530

    feat: parse headers

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,35 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
-    let mut buf = [0u8; 1504];
+    let network_interface = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+    let mut buffer = [0u8; 1504];
     loop {
-        let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
+        let network_bytes = network_interface.recv(&mut buffer[..])?;
+        // let flags = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let ether_protocol = u16::from_be_bytes([buffer[2], buffer[3]]);
+        if ether_protocol != 0x0800 {
+            // ignore packets other than ipv4
             continue;
         }
-        eprintln!(
-            "Read {} (flags: {:x}, proto: {:x}): {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+        match etherparse::Ipv4Slice::from_slice(&buffer[4..network_bytes]) {
+            Ok(p) => {
+                let header = p.header();
+                let source_addr = header.source_addr();
+                let destination_addr = header.destination_addr();
+                let protocol = header.protocol();
+                let ttl = header.ttl();
+                eprintln!(
+                    "{} -> {} Read {:x} bytes of proto: {:?} ttl: {}",
+                    source_addr,
+                    destination_addr,
+                    network_bytes - 4,
+                    protocol,
+                    ttl,
+                )
+            }
+            Err(e) => {
+                eprintln!("Errored packet {:?}", e)
+            }
+        }
     }
 }
